a41bc08979b58a7ccb4caa2f4eacbfe90d0554dd
VThejas <thejasvenkatesh97@gmail.com>
2023-03-22 01:40:46 -0700

    Code cleanup for project.py

--- project.py
+++ project.py
@@ -3,240 +3,148 @@ from operator import mul
 import torch
 from torch import nn
 from torch.nn import functional as F
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv, DenseGCNConv
 from torch_geometric.data import Data
 from torch_geometric.loader import DataLoader
-
-"""
-class nconv(nn.Module):
-    def __init__(self):
-        super(nconv,self).__init__()
-
-    def forward(self,x, A):
-        x = torch.einsum('ncvl,vw->ncwl',(x,A))
-        return x.contiguous()
-
-class linear(nn.Module):
-    def __init__(self,c_in,c_out):
-        super(linear,self).__init__()
-        self.mlp = torch.nn.Conv2d(c_in, c_out, kernel_size=(1, 1), padding=(0,0), stride=(1,1), bias=True)
-
-    def forward(self,x):
-        return self.mlp(x)
-
-class gcn(nn.Module):
-    def __init__(self,c_in,c_out,dropout,support_len=3,order=2):
-        super(gcn,self).__init__()
-        self.nconv = nconv()
-        c_in = (order*support_len+1)*c_in
-        self.mlp = linear(c_in,c_out)
-        self.dropout = dropout
-        self.order = order
-
-    def forward(self,x,support):
-        out = [x]
-        for a in support:
-            x1 = self.nconv(x,a)
-            out.append(x1)
-            for k in range(2, self.order + 1):
-                x2 = self.nconv(x1,a)
-                out.append(x2)
-                x1 = x2
-        
-        # print("Before cat", len(out), out[0].shape)
-
-        h = torch.cat(out,dim=1)
-        # print("h:", h.shape)
-        h = self.mlp(h)
-        # print("after mlp", h.shape)
-        h = F.dropout(h, self.dropout, training=self.training)
-        return h
-"""
-
-class SpatialTemporal(nn.Module):
-    def __init__(self, in_channels, dilation, out_channels=None, dilation_channels=32, dropout=0.3):
-        super(SpatialTemporal, self).__init__()
-
-        self.dropout = dropout
-
-        self.gate1 = nn.Conv2d(in_channels=in_channels,
-                                    out_channels=dilation_channels,
-                                    kernel_size=(1, 2),dilation=dilation)
-            
-        self.gate2 = nn.Conv2d(in_channels=in_channels,
-                                    out_channels=dilation_channels,
-                                    kernel_size=(1, 2),dilation=dilation)
-            
-        self.gcn_conv = GCNConv(in_channels=dilation_channels, out_channels=in_channels)
-        # self.gcn_conv = gcn(dilation_channels,in_channels,dropout,support_len=3)
-        
-        if out_channels != None:
-            self.out = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(1, 1))
-        else:
-            self.out = None
-        
-    def forward(self, input, edge_index, supports=None, edge_weight=None):
-        # (batch_size, t, num_nodes, in_channels)
-        input = input.transpose(-3, -1)
-
-        g1 = self.gate1(input)
-        g2 = self.gate2(input)
-
-        # (batch_size, dilation_channels, num_nodes, t_cur - d[i])
-
-        g1 = F.tanh(g1)
-        g2 = F.sigmoid(g2)
-        out1 = g1 * g2
-
-        # gcn_out = self.gcn_conv(g1 * g2, supports)
-
-        # (batch_size, t_cur - d[i], num_nodes, dilation_channels)
-        g1 = g1.transpose(-3, -1)
-        g2 = g2.transpose(-3, -1)
-        
-        out2 = torch.flatten(g1 * g2, end_dim=-3)
-        edge_index = edge_index.expand(out2.size(0), edge_index.size(-2), edge_index.size(-1))
-        edge_weight = edge_weight.expand(out2.size(0), edge_weight.size(-1)).reshape((out2.size(0), edge_weight.size(-1), 1))
-
-        dataset = [Data(x=x_input, edge_index=e_i, edge_attr=e_w) for x_input, e_i, e_w in zip(out2, edge_index, edge_weight)]
-
-        loader = DataLoader(dataset=dataset, batch_size=out2.size(0))
-
-        data = next(iter(loader))
-
-
-        gcn_out = None
-        cur_shape = list(g1.shape)
-        batch_size = reduce(mul, cur_shape[:-2])
-
-        g1 = torch.reshape(g1, (batch_size, cur_shape[-2], cur_shape[-1]))
-        g2 = torch.reshape(g2, (batch_size, cur_shape[-2], cur_shape[-1]))
-
-
-
-        """for batch in range(len(g1)): 
-            g = self.gcn_conv(g1[batch] * g2[batch], edge_index, edge_weight)
-            if gcn_out == None:
-                gcn_out = torch.unsqueeze(g, 0)
-            else:
-                gcn_out = torch.cat([gcn_out, torch.unsqueeze(g, 0)])"""
-
-        gcn_out = self.gcn_conv(data.x, data.edge_index, edge_weight=torch.flatten(data.edge_attr))
-
-        gcn_out = torch.reshape(gcn_out, tuple(cur_shape[:-1] + [self.gcn_conv.out_channels]))
-        gcn_out = F.dropout(gcn_out, p=self.dropout)
-
-        
-        if self.out != None:
-            gcn_out = gcn_out.transpose(-3, -1)
-            gcn_out = self.out(gcn_out)
-            gcn_out = gcn_out.transpose(-3, -1)  
-
-        
-
-
-        # (batch_size, t_cur - d[i], num_nodes, in_channels)
-
-        
-
-        # (batch_size, out_channels, num_nodes, t-dilation)
-
-        return out1, gcn_out
     
 class GraphWaveNet(nn.Module):
-    def __init__(self, num_nodes, adj, in_channels, out_channels, out_timestamps, dilations=[1,2,1,2,1,2,1,2], dropout=0.3, residual_channels=32, dilation_channels=32, skip_channels=256, supports=None):
+    def __init__(self, 
+                 num_nodes, in_channels, out_channels, out_timesteps, 
+                 dilations=[1, 2, 1, 2, 1, 2, 1, 2], adptive_embeddings=10, dropout=0.3, 
+                 residual_channels=32, dilation_channels=32, skip_channels=256, end_channels=512):
+        
         super(GraphWaveNet, self).__init__()
 
         self.total_dilation = sum(dilations)
+        self.num_dilations = len(dilations)
+        self.num_nodes = num_nodes
+
+        self.dropout = dropout
+
+        self.e1 = nn.Parameter(torch.randn(num_nodes, adptive_embeddings), requires_grad=True)
+        self.e2 = nn.Parameter(torch.randn(adptive_embeddings, num_nodes), requires_grad=True)
+
         self.input = nn.Conv2d(in_channels=in_channels, out_channels=residual_channels, kernel_size=(1, 1))
 
-        self.adj = adj
-        self.num_nodes = num_nodes
-        self.supports = supports
-
-        self.e1 = nn.Parameter(torch.randn(num_nodes, 10), requires_grad=True)
-        self.e2 = nn.Parameter(torch.randn(10, num_nodes), requires_grad=True)
-
-        self.spatial_temporals = nn.ModuleList()
-        self.bns = nn.ModuleList()
-        self.bns2 = nn.BatchNorm2d(skip_channels)
+        self.tcn_a = nn.ModuleList()
+        self.tcn_b = nn.ModuleList()
+        self.gcn = nn.ModuleList()
+        self.gcn_adp = nn.ModuleList()
+        self.bn = nn.ModuleList()
         self.skip = nn.ModuleList()
 
-        self.out_timestamps = out_timestamps
+        self.out_timesteps = out_timesteps
         self.out_channels = out_channels
         
         for d in dilations:
-            self.spatial_temporals.append(SpatialTemporal(residual_channels, d, dilation_channels=dilation_channels, dropout=dropout))
-            self.skip.append(nn.Conv2d(in_channels=residual_channels, out_channels=skip_channels, kernel_size=(1, 1)))
-            self.bns.append(nn.BatchNorm2d(residual_channels))
+            self.tcn_a.append(nn.Conv2d(in_channels=residual_channels,
+                                    out_channels=dilation_channels,
+                                    kernel_size=(1, 2),dilation=d))
             
+            self.tcn_b.append(nn.Conv2d(in_channels=residual_channels,
+                                        out_channels=dilation_channels,
+                                        kernel_size=(1, 2),dilation=d))
+                
+            self.gcn.append(GCNConv(in_channels=dilation_channels, out_channels=residual_channels))
+            self.gcn_adp.append(DenseGCNConv(in_channels=dilation_channels, out_channels=residual_channels))
 
-        self.end_tmp1 = nn.Conv2d(in_channels=skip_channels, out_channels=512, kernel_size=(1, 1))
-        self.end_tmp2 = nn.Conv2d(in_channels=512, out_channels=out_channels * out_timestamps, kernel_size=(1, 1))
-        self.end1 = nn.Conv2d(in_channels=512, out_channels=out_timestamps, kernel_size=(1, 1))
-        self.end2 = nn.Conv2d(in_channels=1, out_channels=out_channels, kernel_size=(1, 1))
+            self.skip.append(nn.Conv2d(in_channels=residual_channels, out_channels=skip_channels, kernel_size=(1, 1)))
+            self.bn.append(nn.BatchNorm2d(residual_channels))      
 
-    def forward(self, x):
-        # (batch_size, t, num_nodes, input_dim)
+        self.end1 = nn.Conv2d(in_channels=skip_channels, out_channels=end_channels, kernel_size=(1, 1))
+        self.end2 = nn.Conv2d(in_channels=end_channels, out_channels=out_channels * out_timesteps, kernel_size=(1, 1))
 
-        edge_list = [[], []]
-        edge_weight = []
+    def forward(self, x, edge_index, edge_weight=None):
+        # x -> (*, t, num_nodes, in_channels)
+        # edge_index -> (2, messages) 
+        # edge_weight -> (messages)
+
+        if len(x.size()) == 3:
+            x = torch.unsqueeze(x, dim=0)
+
+        batch_size = x.size()[:-3]
+        input_timesteps = x.size(-3)
+
+        x = x.transpose(-1, -3)
+
+        if self.total_dilation + 1 > input_timesteps:
+            x = F.pad(x, (self.total_dilation - input_timesteps + 1, 0))
+        
+        x = self.input(x)
 
         adp = F.softmax(F.relu(torch.mm(self.e1, self.e2)), dim=1)
+        edge_index_adp = [[], []]
+        edge_weight_adp = []
         
         for i in range(self.num_nodes):
             for j in range(self.num_nodes):
-                if self.adj.item((i, j)) != 0:
-                    edge_list[0].append(i)
-                    edge_list[1].append(j)
-                    edge_weight.append(self.adj.item((i, j)) + adp[i][j])
+                if adp[i][j] != 0:
+                    edge_index_adp[0].append(i)
+                    edge_index_adp[1].append(j)
+                    edge_weight_adp.append(adp[i][j])
 
-        edge_list = torch.tensor(edge_list)
-        edge_weight = torch.tensor(edge_weight)
-        x = x.transpose(-3, -1)
-
-        # (batch_size, input_dim, num_nodes, t)
-        if self.total_dilation > x.shape[-1]:
-            x = F.pad(x, (self.total_dilation - x.shape[-1] + 1, 0))
-        x = self.input(x)
-
+        edge_index_adp = torch.tensor(edge_index_adp)
+        edge_weight_adp = torch.tensor(edge_weight_adp)
+        
         skip_out = None
 
-        # (batch_size, residual_channels, num_nodes, t)
-        for k in range(len(self.spatial_temporals)):
+        for k in range(self.num_dilations):
             residual = x
 
-            x = x.transpose(-3, -1)
-            # (batch_size, t_cur, num_nodes, residual_channels)
-            out1, x = self.spatial_temporals[k](x, edge_list, supports=self.supports + [adp], edge_weight=edge_weight)
+            g1 = self.tcn_a[k](x)
+            g1 = F.tanh(g1)
 
-            # (batch_size, t_cur-d, num_nodes, residual_channels) 
+            g2 = self.tcn_b[k](x)
+            g2 = F.sigmoid(g2)
 
-            x = x.transpose(-3, -1)
-            x = x + residual[..., -x.shape[-1]:]
-            x = self.bns[k](x)
+            g = g1 * g2
 
-            skip_cur = self.skip[k](out1)
+            skip_cur = self.skip[k](g)
+
             if skip_out == None:
                 skip_out = skip_cur
             else:
-                skip_out = skip_out[..., -skip_cur.shape[-1]:] + skip_cur
-            
+                skip_out = skip_out[..., -skip_cur.size(-1):] + skip_cur
 
+            g = g.transpose(-1, -3)
 
-        # (batch_size, skip_channels, num_nodes, 1) 
+            timesteps = g.size(-3)
+            g = g.reshape(reduce(mul, g.size()[:-2]), * g.size()[-2:])
 
-        x = (self.end_tmp2(F.relu(self.end_tmp1(F.relu(skip_out))))).reshape((-1, self.out_timestamps, self.out_channels, self.num_nodes)).transpose(-1, -2)
-        """x = F.relu(x)
-        x = x.transpose(-3, -1)
+            data = self.batch_timesteps(g, edge_index, edge_weight)
 
-        # (batch_size, 1, num_nodes, out_timestamps) 
+            gcn_out = self.gcn[k](data.x, data.edge_index, edge_weight=torch.flatten(data.edge_attr))
+            gcn_out = gcn_out.reshape(*batch_size, timesteps, -1, self.gcn[k].out_channels)
 
-        x = self.end2(x)
+            gcn_out_adp = self.gcn_adp[k](g, adp)
+            gcn_out_adp = gcn_out_adp.reshape(*batch_size, timesteps, -1, self.gcn[k].out_channels)
+
+            x = gcn_out + gcn_out_adp
+
+            x = F.dropout(x, p=self.dropout)
+
+            x = x.transpose(-3, -1)
+
+            x = x + residual[..., -x.size(-1):]
+            x = self.bn[k](x)
+        
+        skip_out = skip_out[..., -1:]
+
+        x = F.relu(skip_out)
+        x = self.end1(x)
         x = F.relu(x)
-
-        # (batch_size, out_channels, num_nodes, out_timestamps)
-        x = x.transpose(-3, -1)"""
-
+        x = self.end2(x)
+        x = x.reshape(*batch_size, self.out_timesteps, self.out_channels, self.num_nodes).transpose(-1, -2)
         
         return x
+    
+    def batch_timesteps(self, x, edge_index, edge_weight=None):
+        edge_index = edge_index.expand(x.size(0), * edge_index.shape)
+        
+        if edge_weight != None:
+            edge_weight = edge_weight.expand(x.size(0), * edge_weight.shape)
+        
+        dataset = [Data(x=_x, edge_index=e_i, edge_attr=e_w) for _x, e_i, e_w in zip(x, edge_index, edge_weight)]
+        loader = DataLoader(dataset=dataset, batch_size=x.size(0))
+
+        return next(iter(loader))
