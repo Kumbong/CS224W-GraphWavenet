85adb2e0aa9f0f614d44ebff9cc4e293c71f2ca3
Thejas <thejas@DNa80b1e7.SUNet>
2023-03-22 17:38:00 -0700

    Clean up code, add data

--- model.py
+++ model.py
@@ -1,224 +1,56 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.autograd import Variable
-import sys
+import torch.optim as optim
+from graphwavenet import *
+import util
 
+class Model():
+    def __init__(self, scaler, num_nodes, lrate, wdecay, device, adj_mx):
+        self.gwnet = GraphWaveNet(num_nodes, 2, 1, 12)
+        self.gwnet.to(device)
+        self.optimizer = optim.Adam(self.gwnet.parameters(), lr=lrate, weight_decay=wdecay)
+        self.loss = util.masked_mae
+        self.scaler = scaler
+        self.clip = 5
 
-class nconv(nn.Module):
-    def __init__(self):
-        super(nconv,self).__init__()
-
-    def forward(self,x, A):
-        x = torch.einsum('ncvl,vw->ncwl',(x,A))
-        return x.contiguous()
-
-class linear(nn.Module):
-    def __init__(self,c_in,c_out):
-        super(linear,self).__init__()
-        self.mlp = torch.nn.Conv2d(c_in, c_out, kernel_size=(1, 1), padding=(0,0), stride=(1,1), bias=True)
-
-    def forward(self,x):
-        return self.mlp(x)
-
-class gcn(nn.Module):
-    def __init__(self,c_in,c_out,dropout,support_len=3,order=2):
-        super(gcn,self).__init__()
-        self.nconv = nconv()
-        c_in = (order*support_len+1)*c_in
-        self.mlp = linear(c_in,c_out)
-        self.dropout = dropout
-        self.order = order
-
-    def forward(self,x,support):
-        out = [x]
-        for a in support:
-            x1 = self.nconv(x,a)
-            out.append(x1)
-            for k in range(2, self.order + 1):
-                x2 = self.nconv(x1,a)
-                out.append(x2)
-                x1 = x2
+        self.edge_index = [[], []]
+        self.edge_weight = []
         
-        print("Before cat", len(out), out[0].shape)
+        for i in range(num_nodes):
+            for j in range(num_nodes):
+                if adj_mx.item((i, j)) != 0:
+                    self.edge_index[0].append(i)
+                    self.edge_index[1].append(j)
+                    self.edge_weight.append(adj_mx.item((i, j)))
 
-        h = torch.cat(out,dim=1)
-        print("h:", h.shape)
-        h = self.mlp(h)
-        print("after mlp", h.shape)
-        h = F.dropout(h, self.dropout, training=self.training)
-        return h
+        self.edge_index = torch.tensor(self.edge_index)
+        self.edge_weight = torch.tensor(self.edge_weight)
 
+    def train(self, input, real_val):
+        self.gwnet.train()
+        self.optimizer.zero_grad()
+        input = input.transpose(-3, -1)
+        output = self.gwnet(input, self.edge_index, self.edge_weight)
 
-class gwnet(nn.Module):
-    def __init__(self, device, num_nodes, dropout=0.3, supports=None, gcn_bool=True, addaptadj=True, aptinit=None, in_dim=2,out_dim=12,residual_channels=32,dilation_channels=32,skip_channels=256,end_channels=512,kernel_size=2,blocks=4,layers=2):
-        super(gwnet, self).__init__()
-        self.dropout = dropout
-        self.blocks = blocks
-        self.layers = layers
-        self.gcn_bool = gcn_bool
-        self.addaptadj = addaptadj
+        output = output.transpose(-3, -1)
+        real = torch.unsqueeze(real_val,dim=1)
+        predict = self.scaler.inverse_transform(output)
 
-        self.filter_convs = nn.ModuleList()
-        self.gate_convs = nn.ModuleList()
-        self.residual_convs = nn.ModuleList()
-        self.skip_convs = nn.ModuleList()
-        self.bn = nn.ModuleList()
-        self.gconv = nn.ModuleList()
+        loss = self.loss(predict, real, 0.0)
+        loss.backward()
+        if self.clip is not None:
+            torch.nn.utils.clip_grad_norm_(self.gwnet.parameters(), self.clip)
+        self.optimizer.step()
+        mape = util.masked_mape(predict,real,0.0).item()
+        rmse = util.masked_rmse(predict,real,0.0).item()
+        return loss.item(),mape,rmse
 
-        self.start_conv = nn.Conv2d(in_channels=in_dim,
-                                    out_channels=residual_channels,
-                                    kernel_size=(1,1))
-        self.supports = supports
-
-        receptive_field = 1
-
-        self.supports_len = 0
-        if supports is not None:
-            self.supports_len += len(supports)
-
-        if gcn_bool and addaptadj:
-            if aptinit is None:
-                if supports is None:
-                    self.supports = []
-                self.nodevec1 = nn.Parameter(torch.randn(num_nodes, 10).to(device), requires_grad=True).to(device)
-                self.nodevec2 = nn.Parameter(torch.randn(10, num_nodes).to(device), requires_grad=True).to(device)
-                self.supports_len +=1
-            else:
-                if supports is None:
-                    self.supports = []
-                m, p, n = torch.svd(aptinit)
-                initemb1 = torch.mm(m[:, :10], torch.diag(p[:10] ** 0.5))
-                initemb2 = torch.mm(torch.diag(p[:10] ** 0.5), n[:, :10].t())
-                self.nodevec1 = nn.Parameter(initemb1, requires_grad=True).to(device)
-                self.nodevec2 = nn.Parameter(initemb2, requires_grad=True).to(device)
-                self.supports_len += 1
-
-
-
-
-        for b in range(blocks):
-            additional_scope = kernel_size - 1
-            new_dilation = 1
-            for i in range(layers):
-                # dilated convolutions
-                self.filter_convs.append(nn.Conv2d(in_channels=residual_channels,
-                                                   out_channels=dilation_channels,
-                                                   kernel_size=(1,kernel_size),dilation=new_dilation))
-
-                self.gate_convs.append(nn.Conv2d(in_channels=residual_channels,
-                                                 out_channels=dilation_channels,
-                                                 kernel_size=(1, kernel_size), dilation=new_dilation))
-
-                # 1x1 convolution for residual connection
-                self.residual_convs.append(nn.Conv1d(in_channels=dilation_channels,
-                                                     out_channels=residual_channels,
-                                                     kernel_size=(1, 1)))
-
-                # 1x1 convolution for skip connection
-                self.skip_convs.append(nn.Conv2d(in_channels=dilation_channels,
-                                                 out_channels=skip_channels,
-                                                 kernel_size=(1, 1)))
-                self.bn.append(nn.BatchNorm2d(residual_channels))
-                new_dilation *=2
-                receptive_field += additional_scope
-                additional_scope *= 2
-                if self.gcn_bool:
-                    self.gconv.append(gcn(dilation_channels,residual_channels,dropout,support_len=self.supports_len))
-
-
-
-        self.end_conv_1 = nn.Conv2d(in_channels=skip_channels,
-                                  out_channels=end_channels,
-                                  kernel_size=(1,1),
-                                  bias=True)
-
-        self.end_conv_2 = nn.Conv2d(in_channels=end_channels,
-                                    out_channels=out_dim,
-                                    kernel_size=(1,1),
-                                    bias=True)
-
-        self.receptive_field = receptive_field
-
-
-
-    def forward(self, input):
-        in_len = input.size(3)
-        if in_len<self.receptive_field:
-            x = nn.functional.pad(input,(self.receptive_field-in_len,0,0,0))
-        else:
-            x = input
-        print("Start shape", x.shape)
-        x = self.start_conv(x)
-        skip = 0
-
-        print("After input conv shape", x.shape)
-
-        # calculate the current adaptive adj matrix once per iteration
-        new_supports = None
-        if self.gcn_bool and self.addaptadj and self.supports is not None:
-            adp = F.softmax(F.relu(torch.mm(self.nodevec1, self.nodevec2)), dim=1)
-            new_supports = self.supports + [adp]
-
-        # WaveNet layers
-        for i in range(self.blocks * self.layers):
-
-            #            |----------------------------------------|     *residual*
-            #            |                                        |
-            #            |    |-- conv -- tanh --|                |
-            # -> dilate -|----|                  * ----|-- 1x1 -- + -->	*input*
-            #                 |-- conv -- sigm --|     |
-            #                                         1x1
-            #                                          |
-            # ---------------------------------------> + ------------->	*skip*
-
-            #(dilation, init_dilation) = self.dilations[i]
-
-            #residual = dilation_func(x, dilation, init_dilation, i)
-            residual = x
-            print("i, shape", i, residual.shape)
-            # dilated convolution
-            filter = self.filter_convs[i](residual)
-            filter = torch.tanh(filter)
-            gate = self.gate_convs[i](residual)
-            gate = torch.sigmoid(gate)
-            x = filter * gate
-
-            # parametrized skip connection
-
-            s = x
-            s = self.skip_convs[i](s)
-            try:
-                skip = skip[:, :, :,  -s.size(3):]
-            except:
-                skip = 0
-            skip = s + skip
-
-
-            if self.gcn_bool and self.supports is not None:
-                if self.addaptadj:
-                    x = self.gconv[i](x, new_supports)
-                else:
-                    x = self.gconv[i](x,self.supports)
-            else:
-                x = self.residual_convs[i](x)
-
-            x = x + residual[:, :, :, -x.size(3):]
-
-
-            x = self.bn[i](x)
-
-            print("After iter {}, shape is {}".format(i, x.shape))
-
-        x = F.relu(skip)
-        x = F.relu(self.end_conv_1(x))
-        print("After end conv 1, shape is {}".format(x.shape))
-        x = self.end_conv_2(x)
-        print("After end conv 2, shape is {}".format(x.shape))
-
-        return x
-
-
-
-
-
+    def eval(self, input, real_val):
+        self.gwnet.eval()
+        input = input.transpose(-3, -1)
+        output = self.gwnet(input, self.edge_index, self.edge_weight)
+        output = output.transpose(-3, -1)
+        real = torch.unsqueeze(real_val,dim=1)
+        predict = self.scaler.inverse_transform(output)
+        loss = self.loss(predict, real, 0.0)
+        mape = util.masked_mape(predict,real,0.0).item()
+        rmse = util.masked_rmse(predict,real,0.0).item()
+        return loss.item(),mape,rmse
--- util.py
+++ util.py
@@ -5,23 +5,15 @@ import scipy.sparse as sp
 import torch
 from scipy.sparse import linalg
 
-
 class DataLoader(object):
-    def __init__(self, xs, ys, batch_size, pad_with_last_sample=True):
-        """
-        :param xs:
-        :param ys:
-        :param batch_size:
-        :param pad_with_last_sample: pad with the last sample to make number of samples divisible to batch_size.
-        """
+    def __init__(self, xs, ys, batch_size):
         self.batch_size = batch_size
         self.current_ind = 0
-        if pad_with_last_sample:
-            num_padding = (batch_size - (len(xs) % batch_size)) % batch_size
-            x_padding = np.repeat(xs[-1:], num_padding, axis=0)
-            y_padding = np.repeat(ys[-1:], num_padding, axis=0)
-            xs = np.concatenate([xs, x_padding], axis=0)
-            ys = np.concatenate([ys, y_padding], axis=0)
+        num_padding = (batch_size - (len(xs) % batch_size)) % batch_size
+        x_padding = np.repeat(xs[-1:], num_padding, axis=0)
+        y_padding = np.repeat(ys[-1:], num_padding, axis=0)
+        xs = np.concatenate([xs, x_padding], axis=0)
+        ys = np.concatenate([ys, y_padding], axis=0)
         self.size = len(xs)
         self.num_batch = int(self.size // self.batch_size)
         self.xs = xs
@@ -62,8 +54,6 @@ class StandardScaler():
     def inverse_transform(self, data):
         return (data * self.std) + self.mean
 
-
-
 def sym_adj(adj):
     """Symmetrically normalize adjacency matrix."""
     adj = sp.coo_matrix(adj)
@@ -72,42 +62,6 @@ def sym_adj(adj):
     d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
     d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
     return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).astype(np.float32).todense()
-
-def asym_adj(adj):
-    adj = sp.coo_matrix(adj)
-    rowsum = np.array(adj.sum(1)).flatten()
-    d_inv = np.power(rowsum, -1).flatten()
-    d_inv[np.isinf(d_inv)] = 0.
-    d_mat= sp.diags(d_inv)
-    return d_mat.dot(adj).astype(np.float32).todense()
-
-def calculate_normalized_laplacian(adj):
-    """
-    # L = D^-1/2 (D-A) D^-1/2 = I - D^-1/2 A D^-1/2
-    # D = diag(A 1)
-    :param adj:
-    :return:
-    """
-    adj = sp.coo_matrix(adj)
-    d = np.array(adj.sum(1))
-    d_inv_sqrt = np.power(d, -0.5).flatten()
-    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
-    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
-    normalized_laplacian = sp.eye(adj.shape[0]) - adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocoo()
-    return normalized_laplacian
-
-def calculate_scaled_laplacian(adj_mx, lambda_max=2, undirected=True):
-    if undirected:
-        adj_mx = np.maximum.reduce([adj_mx, adj_mx.T])
-    L = calculate_normalized_laplacian(adj_mx)
-    if lambda_max is None:
-        lambda_max, _ = linalg.eigsh(L, 1, which='LM')
-        lambda_max = lambda_max[0]
-    L = sp.csr_matrix(L)
-    M, _ = L.shape
-    I = sp.identity(M, format='csr', dtype=L.dtype)
-    L = (2 / lambda_max * L) - I
-    return L.astype(np.float32).todense()
 
 def load_pickle(pickle_file):
     try:
@@ -121,24 +75,9 @@ def load_pickle(pickle_file):
         raise
     return pickle_data
 
-def load_adj(pkl_filename, adjtype):
+def load_adj(pkl_filename):
     sensor_ids, sensor_id_to_ind, adj_mx = load_pickle(pkl_filename)
-    if adjtype == "scalap":
-        adj = [calculate_scaled_laplacian(adj_mx)]
-    elif adjtype == "normlap":
-        adj = [calculate_normalized_laplacian(adj_mx).astype(np.float32).todense()]
-    elif adjtype == "symnadj":
-        adj = [sym_adj(adj_mx)]
-    elif adjtype == "transition":
-        adj = [asym_adj(adj_mx)]
-    elif adjtype == "doubletransition":
-        adj = [asym_adj(adj_mx), asym_adj(np.transpose(adj_mx))]
-    elif adjtype == "identity":
-        adj = [np.diag(np.ones(adj_mx.shape[0])).astype(np.float32)]
-    else:
-        error = 0
-        assert error, "adj type not defined"
-    return sensor_ids, sensor_id_to_ind, adj
+    return sensor_ids, sensor_id_to_ind, adj_mx
 
 
 def load_dataset(dataset_dir, batch_size, valid_batch_size= None, test_batch_size=None):
@@ -147,8 +86,9 @@ def load_dataset(dataset_dir, batch_size, valid_batch_size= None, test_batch_siz
         cat_data = np.load(os.path.join(dataset_dir, category + '.npz'))
         data['x_' + category] = cat_data['x']
         data['y_' + category] = cat_data['y']
+
     scaler = StandardScaler(mean=data['x_train'][..., 0].mean(), std=data['x_train'][..., 0].std())
-    # Data format
+
     for category in ['train', 'val', 'test']:
         data['x_' + category][..., 0] = scaler.transform(data['x_' + category][..., 0])
     data['train_loader'] = DataLoader(data['x_train'], data['y_train'], batch_size)
@@ -207,5 +147,3 @@ def metric(pred, real):
     mape = masked_mape(pred,real,0.0).item()
     rmse = masked_rmse(pred,real,0.0).item()
     return mae,mape,rmse
-
-
