9f33286dc6c745d322d894c8734e211a17908794
Thejas <thejas@DNa80b1e7.SUNet>
2023-03-22 20:31:00 -0700

    Support download of h5 file

--- generate_data.py
+++ generate_data.py
@@ -7,26 +7,13 @@ import argparse
 import numpy as np
 import os
 import pandas as pd
+import requests
 
 
 def generate_graph_seq2seq_io_data(df,
                                    x_offsets,
                                    y_offsets,
-                                   add_time_in_day=True,
-                                   add_day_in_week=False,
-                                   scaler=None):
-    """
-    Generate samples from
-    :param df:
-    :param x_offsets:
-    :param y_offsets:
-    :param add_time_in_day:
-    :param add_day_in_week:
-    :param scaler:
-    :return:
-    # x: (epoch_size, input_length, num_nodes, input_dim)
-    # y: (epoch_size, output_length, num_nodes, output_dim)
-    """
+                                   add_time_in_day=True):
 
     num_samples, num_nodes = df.shape
     data = np.expand_dims(df.values, axis=-1)
@@ -37,10 +24,6 @@ def generate_graph_seq2seq_io_data(df,
                         1, "D")
         time_in_day = np.tile(time_ind, [1, num_nodes, 1]).transpose((2, 1, 0))
         feature_list.append(time_in_day)
-    if add_day_in_week:
-        dow = df.index.dayofweek
-        dow_tiled = np.tile(dow, [1, num_nodes, 1]).transpose((2, 1, 0))
-        feature_list.append(dow_tiled)
 
     data = np.concatenate(feature_list, axis=-1)
     x, y = [], []
@@ -56,19 +39,28 @@ def generate_graph_seq2seq_io_data(df,
 
 def generate_train_val_test(args):
     seq_length_x, seq_length_y = args.seq_length_x, args.seq_length_y
-    df = pd.read_hdf(args.traffic_df_filename)
+
+    filename = args.traffic_df_filename
+    if filename == None:
+        url = "https://github.com/Kumbong/CS224W-GraphWavenet/blob/main/data/metr-la.h5?raw=true"
+        filename = 'metr-la.h5'
+        f = open(filename, 'wb')
+        f.write(requests.get(url).content)
+        f.close()
+        df = pd.read_hdf(filename)
+        os.remove(filename)
+    else:
+        df = pd.read_hdf(args.traffic_df_filename)
+
     # 0 is the latest observed sample.
     x_offsets = np.sort(
         np.concatenate((np.arange(-(seq_length_x - 1), 1, 1), )))
     # Predict the next one hour
     y_offsets = np.sort(np.arange(args.y_start, (seq_length_y + 1), 1))
-    x, y = generate_graph_seq2seq_io_data(
-        df,
-        x_offsets=x_offsets,
-        y_offsets=y_offsets,
-        add_time_in_day=True,
-        add_day_in_week=args.dow,
-    )
+    x, y = generate_graph_seq2seq_io_data(df,
+                                          x_offsets=x_offsets,
+                                          y_offsets=y_offsets,
+                                          add_time_in_day=True)
 
     num_samples = x.shape[0]
     num_test = round(num_samples * 0.2)
@@ -102,7 +94,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--traffic_df_filename",
         type=str,
-        default="data/metr-la.h5",
+        default=None,
         help="Raw traffic readings.",
     )
     parser.add_argument(
@@ -122,10 +114,6 @@ if __name__ == "__main__":
         type=int,
         default=1,
         help="Y pred start",
-    )
-    parser.add_argument(
-        "--dow",
-        action='store_true',
     )
 
     args = parser.parse_args()
